aa6bd666ac4bf405308f368b7c1e040185feae75
Holger Brandl <holger.brandl@systema.com>
2020-09-30 21:53:39 +0200

    added koin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,23 @@ plugins {
     application
 }
 
-group = "me.brandl"
+group = "com.github.holgerbrandl"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
+
 }
 
 dependencies {
     // cant upgrade to 1.8 because of https://issues.apache.org/jira/browse/CSV-257
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+    implementation("org.koin:koin-core:2.1.6")
 
     testImplementation(kotlin("test-junit"))
+    testImplementation(kotlin("script-runtime"))
 }
 
 tasks.withType<KotlinCompile>() {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "basamil"
+rootProject.name = "desimuk"
 
