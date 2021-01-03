11308eb2987e359daf257c2548995981e389f101
Holger Brandl <holgerbrandl@users.noreply.github.com>
2021-01-04 00:20:12 +0100

    finished basic model

--- simulations/covid19/src/main/kotlin/covid19.kt
+++ simulations/covid19/src/main/kotlin/covid19.kt
@@ -1,8 +1,13 @@
 import org.apache.commons.math3.random.JDKRandomGenerator
 import org.kalasim.*
+import org.kalasim.misc.display
 import org.koin.core.component.get
-import org.koin.core.qualifier.StringQualifier
 import java.util.*
+
+
+var CONTACT_INFECTION_PROBABILITY = 0.3
+const val MIN_CONTACT_DISTANCE = 5
+
 
 data class Position(val x: Double, val y: Double){
 
@@ -14,11 +19,9 @@ data class Position(val x: Double, val y: Double){
     }
 }
 
-// todo calculate infection inzidenz
-val CONTACT_INFECTION_PROBABILITY = 0.1
 
 class Infection(val person: Person):Component(){
-    val cureProb = uniform(7,14)
+    val cureProb = normal(12,4)
 
     init{
         person.sick = true
@@ -26,9 +29,21 @@ class Infection(val person: Person):Component(){
     }
 
     override fun process() = sequence<Component> {
+        // wait for symptoms
+//        hold(normal(4,2)())
+//
+//        sequence<Component>{
+//            person.let{
+//                passivate()
+//            }
+//        }.toList()
+
         hold(cureProb())
         person.sick = false
         get<NumericLevelMonitor>().dec()
+
+        // undo quarantaine
+//        person.activate()
     }
 }
 
@@ -43,10 +58,17 @@ class Person(var position: Position, var sick: Boolean = false): Component(){
 
             // if in contact check for risk for infection
             if(personTracker.hasSickContact(this@Person)){
-                if(Random().nextDouble() > CONTACT_INFECTION_PROBABILITY) {
+                if(CONTACT_INFECTION_PROBABILITY > Random().nextDouble()) {
                     Infection(this@Person)
+
+
+                    // go into quarantine until cured?
+//                    passivate()
                 }
             }
+
+
+            hold(uniform(1,2).sample())
         }
     }
 }
@@ -55,13 +77,12 @@ class PersonTracker {
     val startLocDist = uniform(0, 100, JDKRandomGenerator(42))
     val incrementDist = normal(0.3, 0.1)
 
-    private val MIN_CONTACT_DISTANCE = 1
 
     val population = mutableListOf<Person>()
 
     fun startPosition(): Position  = Position(startLocDist.sample(), startLocDist.sample())
 
-    fun createPerson() = Person(startPosition()).also { population + it }
+    fun createPerson() = Person(startPosition()).also { population.add(it) }
 
     fun increment(p: Position): Position  =
         Position(p.x + incrementDist(), p.x + incrementDist()).withinCityLimits()
@@ -72,7 +93,9 @@ class PersonTracker {
     fun hasSickContact(person: Person): Boolean = population
         .filter{ it.sick }
         .filter{ it != person}
-        .any{ person.position.distance(it.position) < MIN_CONTACT_DISTANCE }
+        .any{
+            person.position.distance(it.position) < MIN_CONTACT_DISTANCE
+        }
 }
 
 
@@ -81,19 +104,24 @@ fun main() {
     declareDependencies {
         add{ PersonTracker() }
         add{ NumericLevelMonitor("sick_monitor") }
-    }.createSimulation{
+    }.createSimulation(){
 
         val populationManager = get<PersonTracker>()
 
         // use a fixed population
         repeat(100){ populationManager.createPerson() }
+        // add a few sick ones
+        repeat(10){ populationManager.createPerson().also { Infection(it) }}
 
+        run(100)
+
+        // mutate virus
+        CONTACT_INFECTION_PROBABILITY = 0.8
         run(100)
 
         // setup level monitor for sick persons
         get<NumericLevelMonitor>().printHistogram()
+        get<NumericLevelMonitor>().display()
 
     }
 }
-
-
