cb0731558eb35ac5e0c55a9994906f9a718fde87
Holger Brandl <holgerbrandl@users.noreply.github.com>
2021-01-17 23:35:32 +0100

    added inline predicate example

---
+++ src/test/kotlin/org/kalasim/scratch/InlinePredicates.kt
@@ -0,0 +1,38 @@
+package org.kalasim.scratch
+
+import org.kalasim.Component
+import org.kalasim.createSimulation
+
+// Objective:
+// dream to just say something like
+// yield wait(car.direction == 'left' and weather='good')
+
+
+fun main() {
+    createSimulation(true) {
+
+        val waiter = object : Component() {
+
+            var isBusy = true
+
+            override fun process() = sequence<Component> {
+                while(true){
+                    isBusy = !isBusy
+                    hold(3)
+                }
+            }
+        }
+
+        val customer = object : Component() {
+            override fun process() = sequence<Component> {
+                waitPredicate{ waiter.isBusy }
+            }
+
+            suspend fun SequenceScope<Component>.waitPredicate(predicate: () -> Boolean) {
+                while(!predicate()) standby()
+            }
+        }
+
+        run(10)
+    }
+}
