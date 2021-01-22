a6a9fb53a84a2a100446a0dbcda768ac45d13414
Holger Brandl <holgerbrandl@users.noreply.github.com>
2021-01-22 17:25:11 +0100

    externalized common-math4 test project

---
+++ modules/commonmath4/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "me.brandl"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven("https://jitpack.io")
+
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    api( "com.github.apache:commons-math:d71b8c93d2")
+
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
---
+++ modules/commonmath4/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "commonmath4"
+
