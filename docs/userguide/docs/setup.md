22723c54806b298cef2058a250f41fad2a415833
Holger Brandl <holgerbrandl@users.noreply.github.com>
2021-11-01 22:59:16 +0100

    v0.6.91 release

--- docs/userguide/docs/setup.md
+++ docs/userguide/docs/setup.md
@@ -7,7 +7,7 @@
 To get started simply add it as a dependency:
 ```
 dependencies {
-    implementation "com.github.holgerbrandl:kalasim:0.6.90"
+    implementation "com.github.holgerbrandl:kalasim:0.6.91"
 }
 ```
 
---
+++ src/test/kotlin/org/kalasim/scratch/DinPhilKscript.kts
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+//@file:DependsOn("com.github.holgerbrandl:kalasim:0.6.90")
+//DEPS com.github.holgerbrandl:kalasim:0.6.90
+
+////DiningPhilosophers.kt
+package org.kalasim.examples
+
+import krangl.*
+import kravis.geomSegment
+import kravis.plot
+import org.kalasim.*
+import org.kalasim.misc.repeat
+import org.koin.core.component.get
+
+
+fun main() {
+    class Fork : Resource()
+
+    class Philosopher(name: String, val leftFork: Fork, val rightFork: Fork) : Component(name) {
+        val thinking = exponential(1)
+        val eating = exponential(1)
+
+        override fun process() = sequence {
+            while(true) {
+                hold(thinking())
+                request(leftFork) {
+                    hold(0.1) // wait before taking the second fork
+                    request(rightFork) {
+                        hold(eating())
+                        log("$name is eating")
+                    }
+                }
+            }
+        }
+    }
+
+    val sim = createSimulation(true) {
+        traceCollector()
+
+        // create forks and resources
+        val names = listOf("Socrates", "Pythagoras", "Plato", "Aristotle")
+        val forks = repeat(names.size) { Fork() } //.repeat().take(names.size + 1).toList()
+
+        names.forEachIndexed { idx, name ->
+            Philosopher(name, forks[idx], forks[(idx + 1).rem(forks.size)])
+        }
+
+        run(50)
+    }
+
+    // Analysis (gather monitoring data (as in simmer:get_mon_arrivals)
+    data class RequestRecord(val requester: String, val timestamp: TickTime, val resource: String, val quantity: Double)
+
+    val tc = sim.get<TraceCollector>()
+    val requests = tc.filterIsInstance<ResourceEvent>().map {
+        val amountDirected = (if(it.type == ResourceEventType.RELEASED) -1 else 1) * it.amount
+        RequestRecord(it.requester.name, it.time, it.resource.name, amountDirected)
+    }
+
+    // transform data into shape suiteable for interval plotting
+    val requestsDf = requests.asDataFrame()
+        .groupBy("requester")
+        .sortedBy("requester", "timestamp")
+        .addColumn("end_time") { it["timestamp"].lag() }
+        .addColumn("state") { rowNumber.map { if(it.rem(2) == 0) "hungry" else "eating" } }
+        .filter { it["quantity"] gt 0 }
+        .ungroup()
+
+    // visualize with kravis
+    requestsDf.plot(x = "timestamp", xend = "end_time", y = "requester", yend = "requester", color = "state")
+        .geomSegment(size = 15.0)
+}
