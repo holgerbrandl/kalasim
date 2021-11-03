6918e224c43733ef7c6a65f710e0f2d6dfa3d09d
Holger Brandl <holgerbrandl@users.noreply.github.com>
2021-11-03 21:34:30 +0100

    moved persistence scratchpad

--- modules/persistence/sim_persistence.md
+++ modules/persistence/sim_persistence.md
@@ -8,3 +8,8 @@ fst --> https://github.com/RuedigerMoeller/fast-serialization/wiki/JSON-serializ
 or use built-in to enable simple branching
 
 **TODO** kyro (modern )
+
+
+https://betterprogramming.pub/the-top-11-trending-kotlin-libraries-for-2021-71dc6d65af5a
+
+> When you think of a JSON parser, only these names come to mind, Jackson, Gson, or Moshi. And those are the most widely used parsers with Spring and Java applications as well. Kotlin serialization is
--- src/test/kotlin/org/kalasim/misc/SaveLoadSimulation.kt
+++ src/test/kotlin/org/kalasim/misc/SaveLoadSimulation.kt
@@ -2,25 +2,29 @@ package org.kalasim.misc
 
 import com.google.gson.Gson
 import com.thoughtworks.xstream.XStream
-import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver
+import com.thoughtworks.xstream.security.AnyTypePermission
 import org.kalasim.Component
 import org.kalasim.Environment
 import org.kalasim.Resource
 import org.kalasim.createSimulation
+import org.kalasim.demo.MM1Queue
+import java.util.*
 
 object XStreamPersistence {
     @JvmStatic
     fun main(args: Array<String>) {
         class Foo() : Component()
 
-        val env = createSimulation {
-            Foo()
-            Resource()
-        }
+//        val env = createSimulation {
+//            Foo()
+//            Resource()
+//        }
+
+        val env = MM1Queue().apply { run(10) }
 
         // save it to xml
         //    https://github.com/x-stream/xstream/issues/101
-        val xstream = XStream(JsonHierarchicalStreamDriver())
+        val xstream = XStream()
 //    val xstream = XStream(JsonHierarchicalStreamDriver())--> does not work because embeed json driver can just WRITE!
 //    xstream.setMode(XStream.NO_REFERENCES)
 
@@ -28,6 +32,7 @@ object XStreamPersistence {
 
 //    env.getKoin()
         println(xstream.toXML(env))
+        xstream.addPermission(AnyTypePermission.ANY); // to prevent https://stackoverflow.com/questions/30812293/com-thoughtworks-xstream-security-forbiddenclassexception
         val restoredEnv: Environment = xstream.fromXML(envXML) as Environment
 
         println("running restored simulation")
@@ -52,6 +57,7 @@ object GsonPersistence {
 //    val xstream = XStream(JsonHierarchicalStreamDriver())--> does not work because embeed json driver can just WRITE!
 //    xstream.setMode(XStream.NO_REFERENCES)
 
+        // fails because of https://stackoverflow.com/questions/43410654/gson-tojson-throws-stackoverflowerror-in-servlet
         val envXML = gson.toJson(env)
 
 //    env.getKoin()
@@ -62,3 +68,62 @@ object GsonPersistence {
         restoredEnv.run(10.0)
     }
 }
+
+object XStreamPQ {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val q: Queue<String> = PriorityQueue { o1, o2 ->
+            compareValuesBy(
+                o1,
+                o2,
+                { it.length },
+                { it.last() })
+        }
+
+
+        q.add("foo")
+
+        val xstream = XStream()
+        val envXML = xstream.toXML(q)
+
+        xstream.addPermission(AnyTypePermission.ANY); // to prevent https://stackoverflow.com/questions/30812293/com-thoughtworks-xstream-security-forbiddenclassexception
+        val qRestored: PriorityQueue<String> = xstream.fromXML(envXML) as PriorityQueue<String>
+
+        qRestored.add("bar")
+
+        println(qRestored)
+
+    }
+}
+
+
+object XStreamPQ2 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val q: Queue<String> = PriorityQueue { o1, o2 ->
+            compareValuesBy(
+                o1,
+                o2,
+                { it.length },
+                { it.last() })
+        }
+
+
+        q.add("foo")
+
+        val xstream = XStream()
+        val envXML = xstream.toXML(q)
+        println(envXML)
+
+        xstream.addPermission(AnyTypePermission.ANY); // to prevent https://stackoverflow.com/questions/30812293/com-thoughtworks-xstream-security-forbiddenclassexception
+
+        val qRestored: PriorityQueue<String> = xstream.fromXML(envXML) as PriorityQueue<String>
+
+
+        qRestored.add("bar")
+
+        println(qRestored)
+
+    }
+}
+
