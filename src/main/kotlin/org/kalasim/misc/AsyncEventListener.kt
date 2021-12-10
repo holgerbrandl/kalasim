0a232fee8ed15695aedf08fdbb07f5074257c4c5
pambrose <pambrose@mac.com>
2021-12-10 13:36:35 -0800

    Add a version of addEventListener() that supports event types and takes a lambda event handler.

--- src/main/kotlin/org/kalasim/misc/AsyncEventListener.kt
+++ src/main/kotlin/org/kalasim/misc/AsyncEventListener.kt
@@ -6,45 +6,18 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import org.kalasim.ComponentStateChangeEvent
-import org.kalasim.EntityCreatedEvent
 import org.kalasim.Event
 import org.kalasim.EventListener
-import org.kalasim.InteractionEvent
-import org.kalasim.MetricEvent
-import org.kalasim.ResourceActivityEvent
-import org.kalasim.ResourceEvent
 
-@DslMarker
-annotation class KalasimDslMarker
+class AsyncEventListener() : EventListener {
+    val eventChannel = Channel<Event>()
 
-@KalasimDslMarker
-fun asyncEventListener(scope: CoroutineScope = GlobalScope, block: CallbackActions.() -> Unit): AsyncEventListener =
-    CallbackActions()
-        .apply(block)
-        .let { callbackActions ->
-            AsyncEventListener(callbackActions, scope)
-        }
-
-class AsyncEventListener(callbacks: CallbackActions, scope: CoroutineScope = GlobalScope) : EventListener {
-    private val eventChannel = Channel<Event>()
-
-    init {
+    inline fun <reified T : Event> start(scope: CoroutineScope = GlobalScope, crossinline block: (event: T) -> Unit) {
         scope.launch {
             eventChannel.receiveAsFlow()
                 .collect { event: Event ->
-                    callbacks.anyEventActions.forEach { it.invoke(event) }
-                    when (event) {
-                        is ComponentStateChangeEvent -> callbacks.componentStateChangeEventActions.forEach {
-                            it.invoke(event)
-                        }
-                        is EntityCreatedEvent -> callbacks.entityCreatedEventActions.forEach { it.invoke(event) }
-                        is InteractionEvent -> callbacks.interactionEventActions.forEach { it.invoke(event) }
-                        is MetricEvent -> callbacks.metricEventActions.forEach { it.invoke(event) }
-                        is ResourceActivityEvent -> callbacks.resourceActivityEventActions.forEach { it.invoke(event) }
-                        is ResourceEvent -> callbacks.resourceEventActions.forEach { it.invoke(event) }
-                        else -> throw IllegalArgumentException("Unknown event type: $event")
-                    }
+                    if (event is T)
+                        block.invoke(event)
                 }
         }
     }
@@ -53,43 +26,5 @@ class AsyncEventListener(callbacks: CallbackActions, scope: CoroutineScope = Glo
         GlobalScope.launch {
             eventChannel.trySend(event)
         }
-    }
-}
-
-class CallbackActions {
-    internal val anyEventActions by lazy { mutableListOf<(event: Event) -> Unit>() }
-    internal val componentStateChangeEventActions by lazy { mutableListOf<(event: ComponentStateChangeEvent) -> Unit>() }
-    internal val entityCreatedEventActions by lazy { mutableListOf<(event: EntityCreatedEvent) -> Unit>() }
-    internal val interactionEventActions by lazy { mutableListOf<(event: InteractionEvent) -> Unit>() }
-    internal val metricEventActions by lazy { mutableListOf<(event: MetricEvent) -> Unit>() }
-    internal val resourceActivityEventActions by lazy { mutableListOf<(event: ResourceActivityEvent) -> Unit>() }
-    internal val resourceEventActions by lazy { mutableListOf<(event: ResourceEvent) -> Unit>() }
-
-    fun onAnyEvent(block: (event: Event) -> Unit) {
-        anyEventActions += block
-    }
-
-    fun onComponentStateChangeEvent(block: (event: ComponentStateChangeEvent) -> Unit) {
-        componentStateChangeEventActions += block
-    }
-
-    fun onEntityCreatedEvent(block: (event: EntityCreatedEvent) -> Unit) {
-        entityCreatedEventActions += block
-    }
-
-    fun onInteractionEvent(block: (event: InteractionEvent) -> Unit) {
-        interactionEventActions += block
-    }
-
-    fun onMetricEvent(block: (event: MetricEvent) -> Unit) {
-        metricEventActions += block
-    }
-
-    fun onResourceActivityEvent(block: (event: ResourceActivityEvent) -> Unit) {
-        resourceActivityEventActions += block
-    }
-
-    fun onResourceEvent(block: (event: ResourceEvent) -> Unit) {
-        resourceEventActions += block
     }
 }
--- src/test/kotlin/org/kalasim/examples/analysis/LogChannelConsumerDsl.kt
+++ src/test/kotlin/org/kalasim/examples/analysis/LogChannelConsumerDsl.kt
@@ -2,22 +2,19 @@ package org.kalasim.examples.analysis
 
 import org.kalasim.Component
 import org.kalasim.ComponentGenerator
+import org.kalasim.InteractionEvent
 import org.kalasim.asDist
 import org.kalasim.createSimulation
-import org.kalasim.misc.asyncEventListener
 
 // create simulation with no default logging
 val sim = createSimulation {
     ComponentGenerator(iat = 1.asDist()) { Component("Car.${it}") }
 
     // add custom log consumer
-    addEventListener(
-        asyncEventListener {
-            onInteractionEvent { event ->
-                if (event.curComponent?.name == "ComponentGenerator.1")
-                    println(event)
-            }
-        })
+    addEventListener<InteractionEvent> { event ->
+        if (event.curComponent?.name == "ComponentGenerator.1")
+            println(event)
+    }
 
     // run the simulation
     run(100)
