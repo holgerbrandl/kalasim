ff3d3f6df362f31509a5d6af07c31265acb6d7fd
Holger Brandl <holgerbrandl@users.noreply.github.com>
2022-01-16 20:23:47 +0100

    Continued call-center

--- src/test/kotlin/org/kalasim/scratch/callcenter/CallCenter.kt
+++ src/test/kotlin/org/kalasim/scratch/callcenter/CallCenter.kt
@@ -1,3 +1,5 @@
+package org.kalasim.scratch.callcenter
+
 import kravis.SessionPrefs
 import kravis.device.SwingPlottingDevice
 import org.kalasim.*
@@ -27,7 +29,7 @@ class Request : Component() {
     }
 }
 
-class ShiftManager : Component() {
+open class ShiftManager : Component() {
     val shiftIt = shiftModel.iterator()
     val callCenter = get<Resource>()
 
@@ -43,18 +45,34 @@ class ShiftManager : Component() {
             ShiftID.WeekEnd -> 0.0
         }
 
+        // wait for end of shift
+        hold(if(currentShift == ShiftID.WeekEnd) 48 else 12)
+    }
+}
+
+class InterruptingShiftManager: ShiftManager() {
+    override fun repeatedProcess() = sequence {
+        val currentShift = shiftIt.next()
+
+        log("starting new shift $currentShift")
+
+        // adjust shift capacity at the beginning of the shift
+        callCenter.capacity = when(currentShift) {
+            ShiftID.A -> 2.0
+            ShiftID.B -> 8.0
+            ShiftID.WeekEnd -> 0.0
+        }
+
         // complete hangover calls from previous shift
         fun shiftLegacy() = callCenter.claimers.components.filter { it.isInterrupted }
 
+        // incrementally resume interrupted tasks while respecting new capacity
         while(shiftLegacy().isNotEmpty() && callCenter.capacity > 0) {
             val numRunning = callCenter.claimers.components.count { it.isScheduled }
             val spareCapacity = max(0, callCenter.capacity.roundToInt() - numRunning)
 
             // resume interrupted tasks from last shift to max out new capacity
             shiftLegacy().take(spareCapacity).forEach { it.resume() }
-
-            val shiftLegacy = shiftLegacy()
-            log("stalling in while ${numRunning} ${spareCapacity} $shiftLegacy")
 
             standby()
         }
@@ -64,16 +82,17 @@ class ShiftManager : Component() {
 
         // stop and reschedule the ongoing tasks
         callCenter.claimers.components.forEach {
-            // detect remaining task time and request this with high prio so that these tasks are picked up next in the upcoming shift
+            // detect remaining task time and request this with high prio so
+            // that these tasks are picked up next in the upcoming shift
             it.interrupt()
         }
     }
 }
 
-abstract class CallCenter(val arrivalRate: Double = 0.2, logEvents: Boolean = true) : Environment(logEvents) {
 
+abstract class CallCenter(val arrivalRate: Double = 0.3, logEvents: Boolean = true) : Environment(logEvents) {
     // not defined at this point
-    abstract val shiftManager :ShiftManager
+    abstract val shiftManager : ShiftManager
 
     val callCenter = dependency { Resource("Call Center") }
 
@@ -88,23 +107,34 @@ fun main() {
         override val shiftManager = ShiftManager()
     }
 
-//    class SimpleCenter: CallCenter() {
-//        override val shiftManager = ShiftManager()
-//    }
-//
-//    val sim = SimpleCenter()
-
-
-
     sim.run(600)
+
+    sim.callCenter.requesters.queueLengthTimeline.display()
 
     SessionPrefs.OUTPUT_DEVICE = SwingPlottingDevice()
     val claimedTimeline = sim.callCenter.claimers.queueLengthTimeline
 
+
     sim.callCenter.requesters.queueLengthTimeline.display().show()
     claimedTimeline.display().show()
 
-// show shift transition
     claimedTimeline.display("A-B", from = 11.tt, to = 13.tt).show()
     claimedTimeline.display("B-A", from = 23.tt, to = 25.tt).show()
+
+
+    // now investigate a more correct manager who will interrupt ongoing tasks
+    val intSim = object: CallCenter() {
+        override val shiftManager = InterruptingShiftManager()
+    }
+
+    intSim.run(600)
+
+
+    // try again but with more customers
+    val highWorkLoadSim = object: CallCenter(arrivalRate = 0.2) {
+        override val shiftManager = InterruptingShiftManager()
+    }
+    highWorkLoadSim.run(600)
+
+    highWorkLoadSim.callCenter.claimers.queueLengthTimeline.display("high B-A", from = 23.tt, to = 25.tt).show()
 }
---
+++ src/test/kotlin/org/kalasim/scratch/callcenter/multiresource/MultiResourceCallCenter.kt
@@ -0,0 +1,85 @@
+package org.kalasim.scratch.callcenter.multiresource
+
+import kravis.SessionPrefs
+import kravis.device.SwingPlottingDevice
+import org.kalasim.*
+import org.kalasim.plot.kravis.display
+
+
+enum class ShiftID { A, B, WeekEnd }
+
+val shiftModel = sequence {
+    while(true) {
+        repeat(5) { yield(ShiftID.A); yield(ShiftID.B) }
+        yield(ShiftID.WeekEnd)
+    }
+}
+
+// model requests with static duration for now once they got hold of an operator
+class Request : Component() {
+    val shiftManager = get<ShiftManager>()
+
+    override fun process() = sequence {
+        request(shiftManager.currentShift, capacityLimitMode = CapacityLimitMode.SCHEDULE) {
+            hold(1)
+        }
+    }
+}
+
+open class ShiftManager : Component() {
+    protected val shiftIt = shiftModel.iterator()
+
+    val shiftA = Resource("ShiftA", capacity = 2)
+    val shiftB = Resource("ShiftA", capacity = 8)
+    val shiftW = Resource("weekend", capacity = 0)
+
+    var currentShift = shiftA
+
+
+    override fun repeatedProcess() = sequence {
+        val nextShiftID = shiftIt.next()
+
+        log("starting new shift $nextShiftID")
+
+        val requests = currentShift.requesters.asSortedList()
+        requests.forEach {
+            it.component.cancel()
+            it.component.activate(process = Request::process)
+        }
+
+        currentShift = when(nextShiftID) {
+            ShiftID.A -> shiftA
+            ShiftID.B -> shiftB
+            ShiftID.WeekEnd -> shiftW
+        }
+
+        // wait for end of shift
+        hold(if(nextShiftID == ShiftID.WeekEnd) 48 else 12)
+    }
+}
+
+
+class MultiResourceCallCenter(arrivalRate: Double = 0.3, logEvents: Boolean = true) : Environment(logEvents) {
+
+    val shiftManager = dependency { ShiftManager() }
+
+    init {
+        ComponentGenerator(iat = exponential(arrivalRate)) { Request() }
+    }
+}
+
+fun main() {
+    val sim = MultiResourceCallCenter()
+
+    sim.run(600)
+
+    SessionPrefs.OUTPUT_DEVICE = SwingPlottingDevice()
+    val aRequesters = sim.shiftManager.shiftA.requesters.queueLengthTimeline
+
+
+    aRequesters.display().show()
+
+// show shift transition
+    aRequesters.display("A-B", from = 11.tt, to = 13.tt).show()
+    aRequesters.display("B-A", from = 23.tt, to = 25.tt).show()
+}
