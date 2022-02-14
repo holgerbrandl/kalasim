284f82fbddeee4e43015def01d913fc7769932be
Holger Brandl <holgerbrandl@users.noreply.github.com>
2022-02-14 08:52:40 +0100

    Simplified clock-sync example

--- src/test/kotlin/org/kalasim/examples/api/RealtimeSimulation.kts
+++ src/test/kotlin/org/kalasim/examples/api/RealtimeSimulation.kts
@@ -5,11 +5,8 @@ import kotlin.time.Duration.Companion.seconds
 val timeBefore = System.currentTimeMillis()
 
 createSimulation(true) {
-    // enable real-time clock synchronization w
+    // enable real-time clock synchronization
     ClockSync(tickDuration = 1.seconds)
-
-    // enable real-time clock synchronization but run in 2x realtime
-//    ClockSync(Duration.ofSeconds(1), speedUp = 2)
 
     run(10)
 }
