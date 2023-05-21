91ca4ff3d791a55cca27b58e58122a3af9bec195
brandl <holgerbrandl@users.noreply.github.com>
2023-05-21 11:44:27 +0200

    improved logging example

--- src/test/kotlin/org/kalasim/examples/api/LoggingAdapter.kts
+++ src/test/kotlin/org/kalasim/examples/api/LoggingAdapter.kts
@@ -2,17 +2,18 @@
 import org.kalasim.analysis.ResourceEvent
 import org.kalasim.examples.er.EmergencyRoom
 import java.util.logging.Logger
-
+import kotlin.time.Duration.Companion.days
 
 // Create simulation
 val er = EmergencyRoom()
 
-val LOG: Logger = Logger.getLogger(this::class.java.getName())
-
 // Add a custom event handler to forward events to the used logging library
-er.addEventListener<ResourceEvent> {
-    LOG.info(it.action?: "")
+er.addEventListener { event->
+    // resolve the event type to a dedicated logger to allow fine-grained control
+    val logger = Logger.getLogger(event::class.java.name)
+
+    logger.info{event.toString()}
 }
 
-// Run the sim for 100 ticks
-er.run(100.0)
+// Run the sim
+er.run(100.days)
