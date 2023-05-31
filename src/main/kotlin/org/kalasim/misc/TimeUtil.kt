c5c1ce815143706b11d3ee8fb320b1e9531f7902
brandl <holgerbrandl@users.noreply.github.com>
2023-05-31 09:29:11 +0200

    added extension to multiply numbers with durations

--- src/main/kotlin/org/kalasim/misc/TimeUtil.kt
+++ src/main/kotlin/org/kalasim/misc/TimeUtil.kt
@@ -12,8 +12,11 @@ import kotlin.time.Duration.Companion.milliseconds
 fun Iterable<Duration>.sum() : Duration = sumOf { it }
 fun <T> Iterable<T>.sumOf(selector: (T) -> Duration) = map{ selector(it).inWholeMilliseconds }.sum().milliseconds
 
+
 fun Iterable<Duration>.mean() : Duration = meanOf { it }
 fun <T> Iterable<T>.meanOf(selector: (T) -> Duration) = map{ selector(it).inWholeMilliseconds}.mean().milliseconds
 
 fun Iterable<Duration>.median() : Duration = meanOf { it }
 fun <T> Iterable<T>.medianOf(selector: (T) -> Duration) = map{ selector(it).inWholeMilliseconds}.median().milliseconds
+
+operator fun Number.times(duration: Duration) = duration.times(this.toDouble())
