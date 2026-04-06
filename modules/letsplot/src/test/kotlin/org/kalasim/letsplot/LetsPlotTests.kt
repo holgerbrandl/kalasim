428e48da24e320256e3a94ddfe9b91b397b24c3a
brandl <holgerbrandl@users.noreply.github.com>
2026-04-06 22:23:26 +0200

    v1.3.4 release

---
+++ modules/letsplot/src/test/kotlin/org/kalasim/letsplot/LetsPlotTests.kt
@@ -0,0 +1,11 @@
+package org.kalasim.letsplot
+
+import org.junit.jupiter.api.Test
+
+class LetsPlotTests {
+
+    @Test
+    fun `test lets plot`() {
+        println("lets plot with kalasim")
+    }
+}
---
+++ modules/notebook/src/test/kotlin/org/kalasim/NotebookTests.kt
@@ -0,0 +1,11 @@
+package org.kalasim
+
+import org.junit.jupiter.api.Test
+
+class NotebookTests {
+
+    @Test
+    fun `test lets plot`() {
+        println("lets plot with kalasim")
+    }
+}
